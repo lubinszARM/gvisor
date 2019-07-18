9a1b23d19f9a2109979cd85698fc19ac7157403b
Ayush Ranjan <ayushranjan@google.com>
2019-07-18 16:40:06 -0700

    ext: Add tests for root directory inode.
    PiperOrigin-RevId: 258870103

--- pkg/sentry/fs/ext/ext.go
+++ pkg/sentry/fs/ext/ext.go
@@ -16,31 +16,56 @@
 package ext
 
 import (
+	"errors"
+	"fmt"
 	"io"
+	"os"
 	"sync"
 
 	"gvisor.dev/gvisor/pkg/abi/linux"
+	"gvisor.dev/gvisor/pkg/sentry/context"
 	"gvisor.dev/gvisor/pkg/sentry/fs/ext/disklayout"
+	"gvisor.dev/gvisor/pkg/sentry/kernel/auth"
+	"gvisor.dev/gvisor/pkg/sentry/vfs"
 	"gvisor.dev/gvisor/pkg/syserror"
 )
 
+// FilesystemType implements vfs.FilesystemType.
+type FilesystemType struct{}
+
+// Compiles only if FilesystemType implements vfs.FilesystemType.
+var _ vfs.FilesystemType = (*FilesystemType)(nil)
+
 // Filesystem implements vfs.FilesystemImpl.
 type Filesystem struct {
-	// dev is the ReadSeeker for the underlying fs device and is protected by mu.
-	dev io.ReadSeeker
+	vfs.FilesystemImpl
 
-	// mu synchronizes the usage of dev. The ext filesystems take locality into
-	// condsideration, i.e. data blocks of a file will tend to be placed close
-	// together. On a spinning disk, locality reduces the amount of movement of
-	// the head hence speeding up IO operations. On an SSD there are no moving
-	// parts but locality increases the size of each transer request. Hence,
-	// having mutual exclusion on the read seeker while reading a file *should*
-	// help in achieving the intended performance gains.
+	vfsfs vfs.Filesystem
+
+	// mu serializes changes to the Dentry tree.
+	mu sync.Mutex
+
+	// dev is the ReadSeeker for the underlying fs device. It is protected by mu
+	// because it is only used when we are making additions to the dentry tree.
+	//
+	// The ext filesystems aim to maximize locality, i.e. place all the data
+	// blocks of a file close together. On a spinning disk, locality reduces the
+	// amount of movement of the head hence speeding up IO operations. On an SSD
+	// there are no moving parts but locality increases the size of each transer
+	// request. Hence, having mutual exclusion on the read seeker while reading a
+	// file *should* help in achieving the intended performance gains.
 	//
 	// Note: This synchronization was not coupled with the ReadSeeker itself
 	// because we want to synchronize across read/seek operations for the
 	// performance gains mentioned above. Helps enforcing one-file-at-a-time IO.
-	mu sync.Mutex
+	dev io.ReadSeeker
+
+	// inodeCache maps absolute inode numbers to the corresponding Inode struct.
+	// Inodes should be removed from this once their reference count hits 0.
+	//
+	// Protected by mu because every addtition and removal from this corresponds to
+	// a change in the dentry tree.
+	inodeCache map[uint32]*Inode
 
 	// sb represents the filesystem superblock. Immutable after initialization.
 	sb disklayout.SuperBlock
@@ -50,26 +75,82 @@ type Filesystem struct {
 	bgs []disklayout.BlockGroup
 }
 
-// newFilesystem is the Filesystem constructor.
-func newFilesystem(dev io.ReadSeeker) (*Filesystem, error) {
-	fs := Filesystem{dev: dev}
-	var err error
+// Compiles only if Filesystem implements vfs.FilesystemImpl.
+var _ vfs.FilesystemImpl = (*Filesystem)(nil)
 
+// getDeviceFd returns the read seeker to the underlying device.
+// Currently there are two ways of mounting an ext(2/3/4) fs:
+//   1. Specify a mount with our internal special MountType in the OCI spec.
+//   2. Expose the device to the container and mount it from application layer.
+func getDeviceFd(source string, opts vfs.NewFilesystemOptions) (io.ReadSeeker, error) {
+	if opts.InternalData == nil {
+		// User mount call.
+		// TODO(b/134676337): Open the device specified by `source` and return that.
+		panic("unimplemented")
+	} else {
+		// NewFilesystem call originated from within the sentry.
+		fd, ok := opts.InternalData.(uintptr)
+		if !ok {
+			return nil, errors.New("internal data for ext fs must be a uintptr containing the file descriptor to device")
+		}
+
+		// We do not close this file because that would close the underlying device
+		// file descriptor (which is required for reading the fs from disk).
+		deviceFile := os.NewFile(fd, source)
+		if deviceFile == nil {
+			return nil, fmt.Errorf("ext4 device file descriptor is not valid: %d", fd)
+		}
+
+		return deviceFile, nil
+	}
+}
+
+// NewFilesystem implements vfs.FilesystemType.NewFilesystem.
+func (fstype FilesystemType) NewFilesystem(ctx context.Context, creds *auth.Credentials, source string, opts vfs.NewFilesystemOptions) (*vfs.Filesystem, *vfs.Dentry, error) {
+	dev, err := getDeviceFd(source, opts)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	fs := Filesystem{dev: dev, inodeCache: make(map[uint32]*Inode)}
+	fs.vfsfs.Init(&fs)
 	fs.sb, err = readSuperBlock(dev)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
 	if fs.sb.Magic() != linux.EXT_SUPER_MAGIC {
 		// mount(2) specifies that EINVAL should be returned if the superblock is
 		// invalid.
-		return nil, syserror.EINVAL
+		return nil, nil, syserror.EINVAL
 	}
 
 	fs.bgs, err = readBlockGroups(dev, fs.sb)
 	if err != nil {
+		return nil, nil, err
+	}
+
+	rootInode, err := fs.getInode(disklayout.RootDirInode)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return &fs.vfsfs, &newDentry(rootInode).vfsd, nil
+}
+
+// getInode gets the inode corresponding to the inode number passed in.
+//
+// Preconditions: must be holding fs.mu.
+func (fs *Filesystem) getInode(inodeNum uint32) (*Inode, error) {
+	if in, ok := fs.inodeCache[inodeNum]; ok {
+		return in, nil
+	}
+
+	in, err := newInode(fs.dev, fs.sb, fs.bgs, inodeNum)
+	if err != nil {
 		return nil, err
 	}
 
-	return &fs, nil
+	fs.inodeCache[inodeNum] = in
+	return in, nil
 }
